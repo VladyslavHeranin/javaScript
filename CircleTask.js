e65686f4ea3ee589bfce6869177a03a6fa0be5fb
VladyslavHeranin <geraninvladik4@gmail.com>
2020-03-30 14:53:52 +0300

    second commit

---
+++ CircleTask.js
@@ -0,0 +1,19 @@
+                            //   ЗАДАЧА НОМЕР ТРИ
+
+
+                            let  radius = Math.floor(Math.random() *91)+10;
+
+                            console.log (radius);
+                            
+                            let circleLength = (radius * Math.PI * 2) ;
+                            
+                            console.log ("длина круга с рандомным радиусом = " + radius + "" );
+                            
+                            console.log (circleLength );
+                            
+                            console.log ("площадь круга с рандомным радиусом  = " + radius + "");
+                            
+                            let areaOfCircle = (Math.PI*(radius*radius));
+                            
+                            console.log (areaOfCircle);
+                            
