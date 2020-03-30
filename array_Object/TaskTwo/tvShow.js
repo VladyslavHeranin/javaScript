4ec7254429070a5656bdf26396c3bee516320f45
VladyslavHeranin <geraninvladik4@gmail.com>
2020-03-30 15:11:21 +0300

    JavaScriptTask

---
+++ array_Object/TaskTwo/tvShow.js
@@ -0,0 +1,18 @@
+                                 //    ЗАДАЧА НОМЕР ДВА 
+
+                                 let tvProgram = ["кохана ми вбиваємо дітей","вагітна в 16"
+                                 ,"хата на тата","міняю жінку","модный приговор"];
+
+                                 console.log(tvProgram);
+                                 
+                                 tvProgram.push(prompt("назва улюбленого шоу"));                                         
+                                                                                                
+                                  console.log(tvProgram);                                                                                                  
+                                                                                                                          
+                                 let favoriteTvProgram = tvProgram.join();                                                  
+                                                                                                                             
+                                 console.log(favoriteTvProgram);
+                                 
+                                 let TheFavoriteTvProgram = tvProgram.join('| ');
+                                 
+                                 console.log(TheFavoriteTvProgram);
