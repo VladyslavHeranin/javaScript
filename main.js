03fbeaf81ecb3c86cd420c51851a042bc969b6cc
VladyslavHeranin <60896490+VladyslavHeranin@users.noreply.github.com>
2020-07-17 14:55:49 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-const squareWidth = 10;
-const heightWidth = 10;
+let Kyiv = document.getElementById('ki');
+let Lviv = document.getElementById('lv');
+let Odesa = document.getElementById('od');
+let Dnipr = document.getElementById('dn');
 
 
-function showCalculatedPerimeter(algoritm, width, height, square) {
-    console.log("Довжина квадрата = " + width);
-    console.log("Ширина квадрата = " + height);
-    console.log("Периметер квадрата = " +  square(width, height));
-    console.log("Площадь квадрата = " + algoritm(width, height));
-  
-};
-showCalculatedPerimeter(dataСhange, squareWidth, heightWidth,changeSqare);
-//пистаи можна код після цього коментарю
+function cityName(x,y) {
+    x.onclick = function () {
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=' + y + '&appid=291115857a205ad90379600be3f21429')
+            .then(function (resp) { return resp.json() })
+            .then(function (data) {
+                console.log(data);
+                document.querySelector(".main__second-title").innerHTML = data.name
+                document.querySelector(".main__second-piont").innerHTML = "temp : " + Math.round(data.main.temp - 273) + "&deg;"
+                document.querySelector(".main__second-state").innerHTML = data.weather[0]["main"];
+                document.querySelector(".main__second-img").innerHTML =
+                    '<img src=" https://openweathermap.org/img/wn/' + data.weather[0]['icon'] + '@2x.png">'
+                document.querySelector(".main__second-data").innerHTML = "wind speed : " + data.wind.speed + " m/sec"
 
-function dataСhange(squareWidth, heightWidth) {
-    return (squareWidth * heightWidth);
-};
+            })
+    }
+}
 
-function changeSqare(squareWidth, heightWidth) {
-    return 2*(squareWidth + heightWidth);
-};
+cityName(Lviv,'Lviv');
+cityName(Kyiv,'Kyiv');
+cityName(Odesa,'Odesa');
+cityName(Dnipr,'Dnepr');
+
+
+
